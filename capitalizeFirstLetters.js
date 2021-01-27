fbb467b2dc1c6add5e9a7cd015abd882901c3b10
xuleasca <florea.roxana5@yahoo.com>
2021-01-27 19:36:32 +0000

    tests

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+
+// WRITE THE ACTUAL FUNCTION HERE
+
+
+let capitalizeFirst = (word) => {
+    return word.length > 0 && (typeof word === 'string') 
+    ? word.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ') 
+    : ''
+      
+       }
+  
+
+// Check that capitalizeFirst is a function
+assert.strictEqual(typeof capitalizeFirst, 'function');
+
+// Check that capitalizeFirst accepts one argument
+assert.strictEqual(capitalizeFirst.length, 1);
+
+// Check that capitalizeFirst transforms javaScript correctly
+assert.strictEqual(capitalizeFirst('java script'), 'Java Script');
+
+//Check string with one word
+assert.strictEqual(capitalizeFirst('bla'), 'Bla');
+
+// Check that it works for a number
+assert.strictEqual(capitalizeFirst(5), '');
+
+// Check that it works for an empty string
+assert.strictEqual(capitalizeFirst(''), '');
+
